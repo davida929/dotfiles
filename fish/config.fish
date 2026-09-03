fcbacb4e782d6d1ecec20f3a3043561d31f83b81
davida929 <davidabertho.93@gmail.com>
2026-09-03 13:42:52 +0100

    support for noctalia shell v5

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+# function wrapper
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	command rm -f -- "$tmp"
+end
+source "$HOME/.cargo/env.fish"
+alias ls="eza --icons"
+alias v="nvim"
+alias t="tmux"
+zoxide init --cmd cd fish | source
+
